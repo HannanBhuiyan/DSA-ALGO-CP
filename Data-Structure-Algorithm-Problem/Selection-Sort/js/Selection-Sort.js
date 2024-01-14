c2a1261ceca881c7083b374f85f05da2e3473fd6
HannanBhuiyan <hannan.diuh@gmail.com>
2024-01-14 09:41:37 -0800

    selection sort part two

--- Data-Structure-Algorithm-Problem/Selection-Sort/js/Selection-Sort.js
+++ Data-Structure-Algorithm-Problem/Selection-Sort/js/Selection-Sort.js
@@ -4,31 +4,30 @@ const numbers = [3, 44, 38, 1, 27, 2, 4]
 // first iteration --> 
 
 
-
 // outer loop for all numbers
+
 for(let i = 0; i < numbers.length - 1; i++) {
 
     let min_index = i
 
-    // inner loop to find the min index
+    // inner loop for find the min index
     for(let j = i + 1; j < numbers.length; j++) {
         if(numbers[j] < numbers[min_index]) {
-           min_index = j
+            min_index = j
         }
-    }
-
-    // swap two numbers i and update min index
+    }  
+    
+    // swap two numbers
     if(min_index != i) {
-        // one way for swap
-        [ numbers[i], numbers[min_index]] = [numbers[min_index], numbers[i]]
-       
-        // another way for swap
+
+        [numbers[i], numbers[min_index]] = [numbers[min_index], numbers[i]]
+
         // let temp = numbers[i]
         // numbers[i] = numbers[min_index]
         // numbers[min_index] = temp
     }
 }
 
-for(let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i])
+for(let i of numbers) {
+    console.log(i)
 }
